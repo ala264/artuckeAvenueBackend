2de6127ab04cd3c10a6c745dc3e0de1c1cb20e73
hussain <hathaull@sfu.ca>
2024-10-10 20:13:55 -0700

    settings update

--- article_app_backend/settings.py
+++ article_app_backend/settings.py
@@ -133,7 +133,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  # Your React app's URL
-    'https://article-avenue-frontend.vercel.app',
+    'https://articleavenue.vercel.app',
 ]
 
 CORS_ALLOW_HEADERS = [
